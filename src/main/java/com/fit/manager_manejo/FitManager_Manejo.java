4cfe94d9d4d60ba0ab869680b2e1b5576d6e7295
Selvyn Ixtabalan Tistoj <estuardotistoj796@gmail.com>
2025-09-06 15:30:25 -0600

    Inicio

---
+++ src/main/java/com/fit/manager_manejo/FitManager_Manejo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fit.manager_manejo;
+
+/**
+ *
+ * @author SELVYN
+ */
+public class FitManager_Manejo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
