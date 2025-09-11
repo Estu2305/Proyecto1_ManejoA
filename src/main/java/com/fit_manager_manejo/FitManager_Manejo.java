c8ed8d9badcbcfa6b3b6b5be274892c342a4dfdb
Selvyn Ixtabalan Tistoj <estuardotistoj796@gmail.com>
2025-09-11 17:35:06 -0600

    Pom

--- src/main/java/com/fit_manager_manejo/FitManager_Manejo.java
+++ src/main/java/com/fit_manager_manejo/FitManager_Manejo.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.fit.manager_manejo;
+package com.fit_manager_manejo;
 
 /**
  *
